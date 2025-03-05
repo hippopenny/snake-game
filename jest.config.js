b883ed917c1ae4e418b15f70f9d7553ce5d3a0a1
tom <tom@hippopenny.com>
2025-03-05 11:49:58 -0800

    config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 export default {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transform: {},
   testMatch: ['**/__tests__/**/*.test.js'],
-  preset: 'jest-playwright-preset',
-  moduleNameMapper: {
-    '@playwright/test': '<rootDir>/node_modules/@playwright/test'
-  }
+  preset: 'jest-playwright-preset'
 };
